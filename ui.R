bb063f5c2bb78478c578c37c3bb6f739a3d7e37e
Darren Wilkinson <darren@algospark.com>
2015-11-17 14:28:17 +0000

    Updated UI to include descriptions

--- ui.R
+++ ui.R
@@ -4,18 +4,22 @@ require(shiny)
 require(leaflet)
 
 shinyUI(pageWithSidebar(
-  headerPanel('Soho Coffee Shop Marketing: Cluster Analysis'),
+  headerPanel('Soho Coffee Shops: Cluster Analysis'),
   sidebarPanel(
-    selectInput('xcol', 'X Variable', names(df2)),
+    h6('This application is for grouping coffee shops in Soho, London. 
+        Choose the number of clusters, and the pairwise clustering terms based on distance from Oxford St., popularity, the number of ratings, distance rank and popularity rank.'),
+    h6('The map below shows the locations of the coffee shops.'),
+    selectInput('xcol', 'X Variable', names(df2),selected=names(df2)[[5]]),
     selectInput('ycol', 'Y Variable', names(df2),
-                selected=names(df2)[[2]]),
-    numericInput('clusters', 'Number of clusters', 2,
+                selected=names(df2)[[4]]),
+    numericInput('clusters', 'Number of clusters', 3,
                  min = 1, max = 6),
+    h6('Rank 1 = nearest (distance) and most popular (popularity).'),
     h6('Notes: distances are from Oxford Circus and calculated as the bird flies minimizing latitude and longitude co-ordinates.'),
     h6('distfactor = coffes shop (lat,lon) - Oxford Circus (lat,lon).'),
     h6('Reviews are the number of reviews as at 16Nov15 on Yelp.com.'),
     h6('Popularity is average weighting divided by the square root of number of reviews.'),
-    h6('Cluster selector does not work on Microsoft Edge, please use another browser such as Firefox or Chrome.')
+       h6('Cluster selector does not work on Microsoft Edge, please use another browser such as Firefox or Chrome.')
                 ),
   mainPanel(
     plotOutput('plot1'),
